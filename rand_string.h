c84b05d9ef4e0a0e3857199acb88b2c201d9b217
panwarsandeep <panwarsandeep@gmail.com>
2015-05-05 14:06:39 +0530

    Create rand_string.h

---
+++ rand_string.h
@@ -0,0 +1,9 @@
+/**
+ * Random string generator.
+ * Copyright (c) 2015 Sandeep Panwar
+ * This software is licensed under the BSD license.
+ * See the accompanying LICENSE.txt for details.
+ *
+ */
+ 
+void get_rand_str(char *str, int len);
